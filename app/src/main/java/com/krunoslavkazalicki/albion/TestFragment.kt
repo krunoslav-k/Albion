ec25681d1b03725abbb54235261dadb5f1e2ef1c
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2022-08-12 13:28:44 +0200

    TestFragment firestore connection implemented and connected to UI

--- app/src/main/java/com/krunoslavkazalicki/albion/TestFragment.kt
+++ app/src/main/java/com/krunoslavkazalicki/albion/TestFragment.kt
@@ -1,6 +1,8 @@
 package com.krunoslavkazalicki.albion
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +10,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class TestFragment : Fragment() {
 
@@ -15,18 +21,29 @@ class TestFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_test, container, false)
         val finishTestButton: Button = view.findViewById(R.id.finishTest_btn)
 
-        val answears1: List<String> = listOf("future I", "future II", "present simple", "past perfect")
-        val answears2: List<String> = listOf("future I", "future II", "present simple", "past perfect")
-        val answears3: List<String> = listOf("future I", "future II", "present simple", "past perfect")
-        val question1 = Question("I will look into it", answears1, "future I")
-        val question2 = Question("I will look into it", answears2, "future I")
-        val question3 = Question("I will look into it", answears3, "future I")
-        val questions: List<Question> = listOf(question1, question2, question3)
+        val db = Firebase.firestore
+        db.collection("TestQuestions")
+                .get()
+                .addOnSuccessListener { result ->
+                    val questions: MutableList<Question> = ArrayList<Question>()
 
-        view.findViewById<RecyclerView>(R.id.questions_rv).apply {
-            layoutManager = LinearLayoutManager(this@TestFragment.context)
-            adapter = QuestionRecyclerAdapter(questions)
-        }
+                    for (document in result){
+                        var questionText = document.data.get("questionText").toString()
+                        var answears: List<String> = document.data.get("answears") as List<String>
+                        var trueAnswear = document.data.get("trueAnswear").toString()
+                        var question = Question(questionText, answears, trueAnswear)
+
+                        questions.add(question)
+                    }
+
+                    view.findViewById<RecyclerView>(R.id.questions_rv).apply {
+                        layoutManager = LinearLayoutManager(this@TestFragment.context)
+                        adapter = QuestionRecyclerAdapter(questions)
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(ContentValues.TAG, "Error getting documents.", exception)
+                }
 
         finishTestButton.setOnClickListener {
 
